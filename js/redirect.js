3b64b230e4b2ce0a2ab2ab54764bc4520eead7f9
Kaarel <karlkaareltaniel.seppel@gmail.com>
2023-12-14 15:52:46 +0200

    Deleteing and adding recipe works

--- js/redirect.js
+++ js/redirect.js
@@ -10,3 +10,26 @@ function navigateToRecipe(recipeId) {
     // Use JavaScript to navigate to the recipe page with the given ID
     window.location.href = `http://localhost:5501/recipe/${recipeId}`;
 }
+
+function navigateToEdit (ID) {
+
+}
+
+async function deleteNavigateToIndex(ID) {
+    try {
+        // Fetch to the server endpoint for deleting a recipe by ID
+        const response = await fetch(`/deleteRecipe/${ID}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            // Deletion successful, navigate to the index or perform any other action
+            navigateToIndex();
+        } else {
+            // Handle errors
+            console.error('Error deleting recipe:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error deleting recipe:', error.message);
+    }
+}
